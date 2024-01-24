b2c83811d6c691b85eadfe056192b15738ea09e7
Atin Gupta <atingupta@email.com>
2024-01-24 19:11:31 +0530

    -

--- cleanup/cleanup-aws-resources.py
+++ cleanup/cleanup-aws-resources.py
@@ -3,10 +3,10 @@ import time
 import traceback
 import json
 
-is_testing = True
+is_testing = False
 
 VERBOSE = 1
-isDryRun = True
+isDryRun = False
 
 def get_client(r_name, r_type):
     client = boto3.client(r_type, region_name=r_name)
@@ -33,10 +33,11 @@ def delete_ec2_instances(c_session):
         all_instances = c_session.describe_instances()
         for reservations in all_instances['Reservations']:
             for instance in reservations['Instances']:
-                instance_id = instance['InstanceId']
-                print(f"Terminating instance - {instance_id}")
-                c_session.terminate_instances(InstanceIds=[instance_id], DryRun=isDryRun)
-                print(f"Terminated instance - {instance_id}")
+                if instance['State']['Name'] != "terminated":
+                    instance_id = instance['InstanceId']
+                    print(f"Terminating instance - {instance_id}")
+                    c_session.terminate_instances(InstanceIds=[instance_id], DryRun=isDryRun)
+                    print(f"Terminated instance - {instance_id}")
     except:
         strError = traceback.format_exc()
 
@@ -79,7 +80,9 @@ def delete_elbv2(c_session):
         aws_resources = c_session.describe_load_balancers()
         for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
             print(f"Deleting aws resource: {aws_resource['LoadBalancerArn']}")
-            c_session.delete_load_balancer(LoadBalancerArn=aws_resource['LoadBalancerArn'], DryRun=isDryRun)
+            if isDryRun:
+                continue
+            c_session.delete_load_balancer(LoadBalancerArn=aws_resource['LoadBalancerArn'])
             print(f"Deleted aws resource: {aws_resource['LoadBalancerArn']}")
     except:
         print(traceback.format_exc())
@@ -120,12 +123,19 @@ def delete_elastic_ips(c_session):
 def delete_asg(c_session):
     print("Starting delete_asg")
     try:
-        aws_resources = c_session.describe_addresses()
+        aws_resources = c_session.describe_auto_scaling_groups()
         for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
-            print(aws_resource)
+            if isDryRun:
+                continue
+
+            #print(aws_resource)
             print(f"Deleting aws resource")
+            r_id = aws_resource['AutoScalingGroupName']
+            c_session.update_auto_scaling_group(AutoScalingGroupName=r_id,
+                                                MinSize=0, MaxSize=0, DesiredCapacity=0)
+
             c_session.delete_auto_scaling_group(AutoScalingGroupName=aws_resource['AutoScalingGroupName'],
-                                                ForceDelete=True, DryRun=isDryRun)
+                                                ForceDelete=True)
             print(f"AWS Resource deleted - {aws_resource['AutoScalingGroupName']}")
     except:
         print(traceback.format_exc())
@@ -201,10 +211,18 @@ def del_rtb(ec2, vpcid):
             for rtb in rtbs:
                 try:
                     assoc_attr = [rtb.associations_attribute for rtb in rtbs]
-                    if [rtb_ass[0]['RouteTableId'] for rtb_ass in assoc_attr if rtb_ass[0]['Main'] == True]:
-                        print(rtb.id + " is the main route table, continue...")
+                    #if [rtb_ass[0]['RouteTableId'] for rtb_ass in assoc_attr if rtb_ass[0]['Main'] == True]:
+                    #    print(rtb.id + " is the main route table, continue...")
                         #continue
                     print("Removing rtb-id: ", rtb.id) if (VERBOSE == 1) else ""
+                    for ass_details in rtb.associations_attribute:
+                        RouteTableAssociationId = ass_details['RouteTableAssociationId']
+                        try:
+                            ec2.disassociate_route_table(AssociationId=RouteTableAssociationId)
+                        except:
+                            pass
+
+
                     table = ec2.RouteTable(rtb.id)
                     table.delete(
                         DryRun = isDryRun
@@ -310,14 +328,6 @@ def delete_vpcs(ec2, c_session):
     print("Starting delete_vpcs")
     try:
         aws_resources = c_session.describe_vpcs()
-        Filters = [
-            {
-                'Name': 'isDefault',
-                'Values': [
-                    'false',
-                ],
-            },
-        ]
 
         for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
             try:
@@ -360,6 +370,7 @@ def delete_vpn_connections(c_session):
 
 
 def delete_ec2_volumes(c_session):
+    print("Starting delete_ec2_volumes")
     aws_resources = c_session.describe_volumes()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
@@ -369,7 +380,10 @@ def delete_ec2_volumes(c_session):
             c_session.delete_volume(VolumeId=r_id, DryRun = isDryRun)
             print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_ec2_volumes")
+
 def delete_ec2_snapshots(c_session):
+    print("Starting delete_ec2_snapshots")
     aws_resources = c_session.describe_snapshots(OwnerIds=["357841589350"], MaxResults=10, )
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
@@ -381,7 +395,11 @@ def delete_ec2_snapshots(c_session):
         except:
             print(traceback.format_exc())
 
+    print("Stopping delete_ec2_snapshots")
+
 def delete_rds_clusters(c_session):
+    print("Starting delete_rds_clusters")
+
     aws_resources = c_session.describe_db_clusters()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
@@ -395,8 +413,12 @@ def delete_rds_clusters(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_rds_clusters")
+
 
 def delete_rds_instances(c_session):
+    print("Starting delete_rds_instances")
+
     aws_resources = c_session.describe_db_instances()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
@@ -410,7 +432,11 @@ def delete_rds_instances(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_rds_instances")
+
 def delete_s3_bucket(c_session):
+    print("Starting delete_s3_bucket")
+
     aws_resources = c_session.list_buckets()
 
     for aws_resource in aws_resources['Buckets']:
@@ -431,27 +457,41 @@ def delete_s3_bucket(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_s3_bucket")
+
 
 def delete_eks(c_session):
+    print("Starting delete_eks")
+
     aws_resources = c_session.list_clusters()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
-        r_id = aws_resource['DBInstanceIdentifier']
-        print(f"Deleting AWS Resource: {r_id}")
-        c_session.delete_db_instance(
-            DBInstanceIdentifier=r_id,
-            SkipFinalSnapshot=True,
-            DeleteAutomatedBackups=True,
-            DryRun=isDryRun,
-        )
-        print(f"Resource deleted - {r_id}")
+        try:
+            r_id = aws_resource['DBInstanceIdentifier']
+            print(f"Deleting AWS Resource: {r_id}")
+            c_session.delete_db_instance(
+                DBInstanceIdentifier=r_id,
+                SkipFinalSnapshot=True,
+                DeleteAutomatedBackups=True,
+                DryRun=isDryRun,
+            )
+            print(f"Resource deleted - {r_id}")
+        except:
+            print(traceback.format_exc())
+
+    print("Stopping delete_eks")
 
 
 
 def delete_cloudwatch_alarms(c_session):
+    print("Starting delete_cloudwatch_alarms")
+
     aws_resources = c_session.describe_alarms()
 
     for aws_cw_alarm in list(aws_resources.keys()):
+        if aws_cw_alarm not in ['CompositeAlarms', 'MetricAlarms']:
+            continue
+
         for aws_resource in aws_resources[aws_cw_alarm]:
             r_id = aws_resource['AlarmName']
             print(f"Deleting AWS Resource: {r_id}")
@@ -464,8 +504,12 @@ def delete_cloudwatch_alarms(c_session):
             )
             print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_cloudwatch_alarms")
+
 
 def delete_sns_topics(c_session):
+    print("Starting delete_sns_topics")
+
     aws_resources = c_session.list_topics()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
@@ -480,12 +524,22 @@ def delete_sns_topics(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_sns_topics")
+
 
 
 def delete_sqs(c_session):
+    print("Starting delete_sqs")
+
     aws_resources = c_session.list_queues()
 
+    if 'QueueUrls' not in list(aws_resources.keys()):
+        return
+
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
+        if aws_resource == "ResponseMetadata":
+            continue
+
         r_id = aws_resource
         print(f"Deleting AWS Resource: {r_id}")
 
@@ -497,8 +551,12 @@ def delete_sqs(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_sqs")
+
 
 def delete_rds_cluster_snapshots(c_session):
+    print("Starting delete_rds_cluster_snapshots")
+
     aws_resources = c_session.describe_db_cluster_snapshots()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
@@ -513,8 +571,12 @@ def delete_rds_cluster_snapshots(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_rds_cluster_snapshots")
+
 
 def delete_rds_snapshots(c_session):
+    print("Starting delete_rds_snapshots")
+
     aws_resources = c_session.describe_db_snapshots()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
@@ -529,11 +591,18 @@ def delete_rds_snapshots(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_rds_snapshots")
+
 
 def delete_customer_gateways(c_session):
+    print("Starting delete_customer_gateways")
+
     aws_resources = c_session.describe_customer_gateways()
 
     for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
+        if aws_resource['State'] == "deleted":
+            continue
+
         r_id = aws_resource['CustomerGatewayId']
         print(f"Deleting AWS Resource: {r_id}")
 
@@ -546,6 +615,50 @@ def delete_customer_gateways(c_session):
         )
         print(f"Resource deleted - {r_id}")
 
+    print("Stopping delete_customer_gateways")
+
+
+def delete_ec2_target_groups(c_session):
+    print("Starting delete_ec2_target_groups")
+
+    aws_resources = c_session.describe_target_groups()
+
+    for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
+        r_id = aws_resource['TargetGroupArn']
+        print(f"Deleting AWS Resource: {r_id}")
+
+        if isDryRun:
+            continue
+
+        c_session.delete_target_group(
+            TargetGroupArn=r_id,
+        )
+        print(f"Resource deleted - {r_id}")
+
+    print("Stopping delete_ec2_target_groups")
+
+
+def delete_security_groups(c_session):
+    print("Starting delete_security_groups")
+
+    aws_resources = c_session.describe_security_groups()
+
+    for aws_resource in aws_resources[list(aws_resources.keys())[0]]:
+        r_id = aws_resource['GroupName']
+
+        if "default" in r_id.lower():
+            continue
+
+        print(f"Deleting AWS Resource: {r_id}")
+
+
+        c_session.delete_security_group(
+            GroupName=r_id, DryRun=isDryRun
+        )
+        print(f"Resource deleted - {r_id}")
+
+    print("Stopping delete_security_groups")
+
 
 if __name__ == "__main__" and not is_testing:
     ec2_client = boto3.client('ec2')
@@ -554,47 +667,69 @@ if __name__ == "__main__" and not is_testing:
     sure = "No"
     if not isDryRun:
         print("All the resources in all the regions will get deleted")
-        sure = input("Are you sure to proceed? (Yes/ No")
+        sure = input("Are you sure to proceed? (Yes/ No): ")
         if sure != "Yes":
             exit()
     else:
         sure = "Yes"
 
     for r_name in all_regions:
+        #if r_name != "us-east-1":
+        #    continue
+
         if sure != "Yes":
             continue
         try:
             # time.sleep(1)
             print(f"----------------------Working with region: {r_name}")
-            c_session = get_client(r_name, 'ec2')
+            c_session = get_client(r_name, 'elbv2')
+            delete_elbv2(c_session)
+
+            c_session = get_client(r_name, 'autoscaling')
             delete_asg(c_session)
+
             c_session = get_client(r_name, 'ec2')
             delete_ec2_instances(c_session)
             delete_ec2_amis(c_session)
             delete_ec2_snapshots(c_session)
-            c_session = get_client(r_name, 'elbv2')
-            delete_elbv2(c_session)
+
             c_session = get_client(r_name, 'ec2')
             delete_nat_gateways(c_session)
             delete_elastic_ips(c_session)
             delete_vpn_connections(c_session)
             detach_vpn_gateways(c_session)
+
             ec2 = boto3.resource('ec2', region_name=r_name)
+            c_session = get_client(r_name, 'ec2')
             delete_vpcs(ec2, c_session)
+
+            c_session = get_client(r_name, 'ec2')
             delete_ec2_volumes(c_session)
-            delete_eks(c_session)
+
+            c_session = get_client(r_name, 'rds')
             delete_rds_instances(c_session)
             delete_rds_clusters(c_session)
-            c_session = get_client('us-east-1', 's3')
+
+            c_session = get_client(r_name, 's3')
             delete_s3_bucket(c_session)
-            c_session = get_client('us-east-1', 'cloudwatch')
+
+            c_session = get_client(r_name, 'cloudwatch')
             delete_cloudwatch_alarms(c_session)
-            c_session = get_client('us-east-1', 'sns')
+
+            c_session = get_client(r_name, 'sns')
             delete_sns_topics(c_session)
-            c_session = get_client('us-east-1', 'sqs')
+
+            c_session = get_client(r_name, 'sqs')
             delete_sqs(c_session)
-            c_session = get_client('us-east-1', 'ec2')
+
+            c_session = get_client(r_name, 'ec2')
             delete_customer_gateways(c_session)
+
+            c_session = get_client(r_name, 'elbv2')
+            delete_ec2_target_groups(c_session)
+
+            c_session = get_client(r_name, 'ec2')
+            delete_security_groups(c_session)
         except:
             print(f"Error while working with region {r_name}")
 
@@ -603,5 +738,15 @@ if __name__ == "__main__" and not is_testing:
 
 
 if is_testing:
-    c_session = get_client('us-east-1', 'eks')
-    delete_eks(c_session)
+    #c_session = get_client('us-east-1', 'eks')
+    #delete_eks(c_session)
+
+    c_session = get_client("us-east-1", 'autoscaling')
+    delete_asg(c_session)
+
+    ec2 = boto3.resource('ec2', region_name="us-east-1")
+    c_session = get_client("us-east-1", 'ec2')
+    delete_vpcs(ec2, c_session)
+
+
+
